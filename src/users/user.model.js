b14ca2f977f4fb916f80a3970f61f82c033c6b69
jchanquin-2022240 <jchanquin-2022240@kinal.edu.gt>
2024-03-02 17:55:27 -0600

    model structure creation

---
+++ src/users/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema({
+    username:{
+        type: String,
+        required: [true, "Username is required"],
+        unique: true
+    },
+    email:{
+        type: String,
+        required: [true, "Email is required"],
+        unique: true
+    },
+    password:{
+        type: String,
+        required: [true, "Password is required"]
+    },
+    userstatus:{
+        type: Boolean,
+        default: true
+    }
+});
+
+
+UserSchema.methods.toJSON = funtion() {
+    const { __v, password, _id, ...user} = this.toObject();
+    user.uid = _id;
+    return user;
+}
+
+export default mongoose.model('User', UserSchema);
