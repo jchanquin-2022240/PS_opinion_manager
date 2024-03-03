f2fc767562f5e6cc333557bb442065fec1ee4867
jchanquin-2022240 <jchanquin-2022240@kinal.edu.gt>
2024-03-03 09:49:10 -0600

    generar-jwt

---
+++ src/helpers/generate-jwt.js
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken';
+
+export const generateJWT = (uid = '') => {
+    return new Promise((resolve, reject) => {
+        const payload = { uid };
+        jwt.sign(
+            payload,
+            proces.env.SECRETORPRIVATEKEY,
+            {
+                expiresIn: '2h'
+            },
+            (error, token) => {
+                error ? (console.log(error),reject('Token not generated')): resolve(token);
+            }
+        );
+    });
+}
