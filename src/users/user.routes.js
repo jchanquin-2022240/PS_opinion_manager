7d76d2a8147f533ecb36c79b3e8a4ae8c28cb912
jchanquin-2022240 <jchanquin-2022240@kinal.edu.gt>
2024-03-02 19:25:25 -0600

    creation structure routes User

--- src/users/user.routes.js
+++ src/users/user.routes.js
@@ -1,4 +1,4 @@
-import { Routes } from "express";
+import { Router } from "express";
 import { check } from "express-validator";
 
 import {
@@ -6,6 +6,20 @@ import {
 } from "./user.controller.js";
 
 import { validateFields } from "../middlewares/validate-fields.js";
+import { existingEmail, existingUsername } from "../helpers/db-validators.js";
 
 
 const router = Router ();
+
+router.post(
+    "/",
+    [
+        check('username', "Username cannot be empty").not().isEmpty(),
+        check('username').custom(existingUsername),
+        check('email', "Email cannot be empty").isEmail(),
+        check('email').custom(existingEmail),
+        check('password', "password must be greater than 8 character").isLength({min: 8}),
+        validateFields,
+    ], userPost);
+
+export default router;
