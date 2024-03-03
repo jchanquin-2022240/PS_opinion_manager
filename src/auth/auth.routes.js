f34a0a037fa0de90c6f8fc652842286698354f18
jchanquin-2022240 <jchanquin-2022240@kinal.edu.gt>
2024-03-02 22:57:31 -0600

    structure auth.routes

---
+++ src/auth/auth.routes.js
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import { check } from "express-validator";
+
+import {
+    login
+} from "./auth.controlle.js";
+
+import { validateFields } from '../middlewares/validate-fields.js';
+
+const router = Router();
+
+router.post(
+    "/loginEmail",
+    [
+        check('email', 'Invalid email').isEmpty(),
+        check('password', 'Password is mandatory').not().isEmpty(),
+        validateFields,
+    ], login);
+
+router.post(
+    "/logUsername",
+    [
+        check('username', 'Username cannot be empty').not().isEmpty(),
+        check('password', 'The password is mandatory').not().isEmpty(),
+        validateFields,
+    ], login)
+    
+export default router;
